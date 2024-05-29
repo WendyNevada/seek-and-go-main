576f2953180967eebc207e98a3ebe9180d29df54
disnoots <felix.daniel55555@gmail.com>
2024-05-29 22:56:49 +0700

    Felix.undo testing storage link

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        base_path('/public_html/storage') => storage_path('app/public'),
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
