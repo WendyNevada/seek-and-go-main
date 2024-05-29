de51549ec03e3622a8ee99af7654fb0dfdcbfa45
disnoots <felix.daniel55555@gmail.com>
2024-05-29 22:51:21 +0700

    Felix.Test storage 2

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('/public_html/storage') => storage_path('app/public'),
+        base_path('/public_html/storage') => storage_path('app/public'),
     ],
 
 ];
